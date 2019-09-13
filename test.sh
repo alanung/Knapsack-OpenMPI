fca8df9ecea9a9a781ef54d82a416d6d843f6ac5
Qifan Deng <qifand@student.unimelb.edu.au>
2019-09-13 22:19:43 +1000

    added test script

---
+++ test.sh
@@ -0,0 +1,14 @@
+test() {
+  ./build.sh
+  Problem=$(./bin/generator $1 $2 $3)
+  echo -e $Problem
+  echo $Problem | ./bin/knapsack
+}
+
+if [ ! $1 ] || [ ! $2 ] || [ ! $3 ] ; then
+  echo -e "Usage:\r\n  ./tesh.sh \$C \$n \$mean\r\n"
+  echo -e "Example:\r\n  tesh.sh 10 7 8\r\n"
+
+else
+  test $1 $2 $3
+fi
