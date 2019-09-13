f52b89cd1e8879f6723aba9c3accffb9d242b997
Qifan Deng <qifand@student.unimelb.edu.au>
2019-09-13 21:03:01 +1000

    added build script and readme

---
+++ build.sh
@@ -0,0 +1,7 @@
+echo -e "[*] Compiling knapsack.c to bin/knapsack ... \c"
+mpicc src/orig/knapsack.c bin/knapsack
+echo -e "done."
+
+echo -e "[*] Compiling generator.c to bin/generator ... \c"
+gcc util/generator.c -lm -o bin/generator
+echo -e "done."
