112002380473b4b03f9878cd2535e312440e9bc4
Qifan Deng <qifand@student.unimelb.edu.au>
2019-09-16 23:22:41 +1000

    edited test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 test() {
   ./build.sh >/dev/null
   echo "The knapsack-dp-mpi:"
-  ./bin/genknap $1 $2 $3 $4 13 | mpirun ./bin/knapsack-dp-mpi
+  ./bin/genknap $1 $2 $3 $4 13 >tmp.in
+  mpirun ./bin/knapsack-dp-mpi <tmp.bin
+  rm tmp.in
 }
 
 for ((i = $1; i < $2 + 1; i += $3)); do
