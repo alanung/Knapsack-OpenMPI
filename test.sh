091370c05249a47969d086b1ca396a04cd4203fb
Qifan Deng <qifand@student.unimelb.edu.au>
2019-09-16 23:18:01 +1000

    edited test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,8 @@
 test() {
-  ./build.sh > /dev/null
-  ./bin/genknap $1 $2 $3 $4 101010 >in_sadfasdf
-  #  cat in_sadfasdf
-  echo "The knapsack-dp-seq:"
-  cat in_sadfasdf | mpirun ./bin/knapsack-dp-seq
-
+  ./build.sh >/dev/null
   echo "The knapsack-dp-mpi:"
-  cat in_sadfasdf | mpirun ./bin/knapsack-dp-mpi -np 4
-  rm in_sadfasdf
+  ./bin/genknap $1 $2 $3 $4 13 | mpirun ./bin/knapsack-dp-mpi
 }
-
-capacity=0
 
 for ((i = $1; i < $2 + 1; i += $3)); do
   echo "    Testing $i ... "
